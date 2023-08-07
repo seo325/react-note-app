cbb8217cbf6eaec05b9400cd727e5292ac6f50a5
seohwa0325 <seohwa0325@gmail.com>
2023-08-07 14:33:47 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+export {default as AllNotes} from './AllNotes/AllNotes'
+export {default as ArchiveNotes} from './ArchiveNotes/ArchiveNotes'
+export {default as ErrorPage} from './ErrorPage/ErrorPage'
+export {default as TagNotes} from './TagNotes/TagNotes'
+export {default as TrashNotes} from './TrashNotes/TrashNotes'
+
